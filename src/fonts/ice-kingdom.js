78b28fc6db08147b072671751e1748c5e6d61aa5
Wassim <wassimhachemi8@gmail.com>
2025-03-26 00:22:26 +0100

    updated some bullshit and added font and photos folders nothing important

--- src/fonts/ice-kingdom.js
+++ src/fonts/ice-kingdom.js
@@ -1,4 +1,3 @@
-// This is a placeholder for the actual font file
-// In a real project, you would place the actual .woff and .woff2 font files in this directory
+
 console.log("ICE KINGDOM font loaded")
 
